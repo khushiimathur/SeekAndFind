395502240dc75f143e5ac1655555ee875ac617fc
Khushi Mathur <khushi098btcse@igdtuw.ac.in>
2024-09-18 20:09:57 +0530

    lol

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ import os
 
 def send_email(recipient_email):
     otp = random.randint(100000, 999999)
-    sender_email = "seekandfindigdtuw@gmail.com"
-    sender_password = "Seekandfind@0627"
+    sender_email = "mathurkhushi027@gmail.com"
+    sender_password = "uhrg plsv afhl kxho"
     subject = "OTP for login to SeekAndFind - IGDTUW"
     body = '''Hello,
     Thank you for logging into SeekAndFind - IGDTUW. Please enter this OTP to verify your credentials.
